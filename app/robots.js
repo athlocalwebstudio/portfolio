c05a3d65c6171b17aed18ae50d73dfbb04769595
athlocalwebstudio <localwebstudio@gmail.com>
2026-04-15 22:47:43 +0300

    add robots.txt

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://www.athlocalwebstudio.com/sitemap.xml",
+  };
+}
